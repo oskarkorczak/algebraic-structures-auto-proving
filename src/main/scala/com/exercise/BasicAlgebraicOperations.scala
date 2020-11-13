fab34bfba64291dd0205c4e0571588629a052022
Oskar Korczak <o.korczak@gmail.com>
2020-11-13 12:53:26 +0000

    Rename add test

--- src/main/scala/com/exercise/BasicAlgebraicOperations.scala
+++ src/main/scala/com/exercise/BasicAlgebraicOperations.scala
@@ -5,5 +5,4 @@ object BasicAlgebraicOperations {
   def add(a: Int, b: Int): Int = {
     a + b
   }
-
 }
--- src/test/scala/com/exercise/BasicAlgebraicOperationsSpec.scala
+++ src/test/scala/com/exercise/BasicAlgebraicOperationsSpec.scala
@@ -1,12 +1,12 @@
 package com.exercise
 
+import com.exercise.BasicAlgebraicOperations.add
 import org.scalacheck.Prop.forAll
 import org.scalacheck.Properties
 
 object BasicAlgebraicOperationsSpec extends Properties("Basic algebraic operations") {
 
-  property("add") = forAll { (a: Int, b: Int) =>
-    BasicAlgebraicOperations.add(a, b) == a + b
+  property("add generates correct result") = forAll { (a: Int, b: Int) =>
+    add(a, b) == a + b
   }
-
 }
