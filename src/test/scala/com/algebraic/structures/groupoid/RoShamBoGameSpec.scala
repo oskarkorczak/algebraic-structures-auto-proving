eb7ce7e4bc6d6e96d6b5ca831482479f21541aa7
Oskar Korczak <o.korczak@gmail.com>
2020-11-14 11:11:43 +0000

    Add RoShamBo counterexample for associativity

--- src/test/scala/com/algebraic/structures/groupoid/RoShamBoGameSpec.scala
+++ src/test/scala/com/algebraic/structures/groupoid/RoShamBoGameSpec.scala
@@ -1,7 +1,7 @@
 package com.algebraic.structures.groupoid
 
 import com.algebraic.structures.groupoid.RoShamBoGame.throwIter
-import org.scalacheck.Prop.forAll
+import org.scalacheck.Prop.{exists, forAll}
 import org.scalacheck.{Gen, Properties}
 
 object RoShamBoGameSpec extends Properties("Rock Paper Scissors") {
@@ -11,5 +11,9 @@ object RoShamBoGameSpec extends Properties("Rock Paper Scissors") {
   property("is total") = forAll(rps, rps) { (a: RoShamBo, b: RoShamBo) =>
     val domain = Vector(Rock, Paper, Scissors)
     domain.contains(throwIter(a, b))
+  }
+  
+  property("is not associative (counterexample)") = exists(rps) { a: RoShamBo =>
+    throwIter(throwIter(a, Rock), Paper) != throwIter(a, throwIter(Rock, Paper))
   }
 }
